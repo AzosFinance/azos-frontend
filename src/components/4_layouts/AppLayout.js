c6fc8c36181c6360cadf85ee89d694fb91f5b6c9
c0mput3rxz <c0mput3rxz@gmail.com>
2023-11-12 02:51:17 -0800

    - add a colorMode aware color gradient sample

--- src/components/4_layouts/AppLayout.js
+++ src/components/4_layouts/AppLayout.js
@@ -1,10 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import {Flex, useColorMode} from "@chakra-ui/react";
 import Navbar from "../3_organisms/Navbar/Navbar";
 import Head from "next/head";
 import Footer from "../1_atoms/Footer/Footer";
 
 const AppLayout = ({ children }) => {
-  return (
+    const { colorMode } = useColorMode();
+
+    return (
     <>
       <Head>
         <title>Azos</title>
@@ -20,7 +22,16 @@ const AppLayout = ({ children }) => {
         justifyContent="space-between"
       >
         <Navbar />
-        <Flex pb="1rem" w="100%" mt="1rem">
+        <Flex pb="1rem" w="100%" mt="1rem"
+              bgGradient={colorMode === "light" ? [
+                  'linear(to-tr, gray.50, gray.100)',
+                  'linear(to-tl, gray.200, gray.500)',
+                  'linear(to-tl, gray.200, gray.100)',
+              ]: [
+                  'linear(to-tr, gray.300, gray.400)',
+                  'linear(to-t, gray.500, gray.600)',
+                  'linear(to-b, gray.700, gray.800)',
+              ]}>
           {children}
         </Flex>
         <Footer />
